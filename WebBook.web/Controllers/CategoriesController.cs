fda437bfffa20177222ac7662fbb7b37aa9ab497
AhmedThakeb <Ahmed.Thakeb@gmail.com>
2024-10-30 21:35:08 +0300

    made a Card for the Categories

---
+++ WebBook.web/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebBook.web.Controllers
+{
+	public class CategoriesController : Controller
+	{
+		private readonly ApplicationDbContext db;
+
+		public CategoriesController(ApplicationDbContext db)
+        {
+			this.db = db;
+		}
+        public IActionResult Index()
+		{
+			//TODO: use VM
+			var categories = db.Categories;
+			return View(categories);
+		}
+	}
+}
